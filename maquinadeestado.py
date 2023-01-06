bf99f632fdc432ac0e62984ef08568b16ad9d859
kalbert contreras <kalbert.contreras@gmail.com>
2023-01-06 11:00:46 -0300

    maquina de estado con tiempos y variables

---
+++ maquinadeestado.py
@@ -0,0 +1,73 @@
+import time
+
+# Definir las cantidades de tiempo en segundos para los estados 2 y 3
+time1 = 1
+time2 = 8 
+time3 = 5
+time4 = 10
+
+last_input = ""
+# Iniciar el estado en 1
+state = 0
+
+start_time=time.time() #tiempo actual
+
+
+# Bucle infinito
+while True:
+
+    if state == 0:
+        print("Entering state 0")
+        # Leer una entrada del usuario
+        user_input = input()
+
+        # Si el usuario escribe "R", cambiar al estado 1 si no, cambia al 3
+        if user_input == "R":
+            state = 1
+
+        elif user_input == "M":
+            state= 3
+    # Si el estado es 1
+    elif state == 1:
+        print("Entering state 1")
+        # Leer una entrada del usuario
+        time.sleep(time1)
+
+        state = 2
+
+    # Si el estado es 2
+    elif state == 2:
+        if last_input or time.time() - start_time > time2:
+        # Mostrar el estado 2
+            print("Entering state 2")
+            user_input = input()
+            time.sleep(8)
+            last_input = user_input
+            print("ingreso por fin")
+            if user_input == "R" :
+                state = 1
+
+            elif user_input == "":
+                state= 0
+
+    # Si el estado es 3
+    elif state == 3:
+        # Mostrar el estado 3
+        print("Entering state 3")
+
+        # Esperar la cantidad de tiempo 2
+        time.sleep(time3)
+
+        # Cambiar al estado 4
+        state = 4
+
+    elif state == 4:
+        # Mostrar el estado 3
+        print("Entering state 4")
+        user_input = input()
+
+        if user_input == "R":
+            state = 3
+
+        elif time4 <= 10:
+            state= 0
--- maquinadeestado2.py
+++ maquinadeestado2.py
@@ -2,38 +2,53 @@ import time
 
 def transition(state, event, elapsed_time):
   if state in transitions:
-    if elapsed_time > transitions[state]['time']:
         if event in transitions[state]:
-            return transitions[state][event]['next_state']
+            return transitions[state][event]
   return state
+start_time = time.time()
+print (start_time)
+elapsed_time = time.time() - start_time
 
+start_estado= time.time() - elapsed_time - elapsed_time + elapsed_time
+time.sleep(1)
+
+print (elapsed_time)
 def on_enter_state0():
-  print("Entering state 0")
+  print("Entering state 0") 
+  print ("llega aqui")
 
 def on_enter_state1():
   print("Entering state 1")
+  print ("llega aqui")
+  print (elapsed_time)
+  print(start_estado)
+  time.sleep(5)
+  print(start_estado)
 
 def on_enter_state2():
   print("Entering state 2")
+  print("llego aqui")
 
 def on_enter_state3():
   print("Entering state 3")
+  print("llego aqui")
 
 def on_enter_state4():
   print("Entering state 4")
+  print ("llego aqui")
 
 transitions = {
   'state0': {'R': 'state1', 'M': 'state3'},
-  'state1': {'time': 1, 'next_state': 'state2'},
-  'state2': {'R': 'state1', 'time': 8, 'next_state': 'state0'},
-  'state3': {'time': 5, 'next_state': 'state4'},
+  'state1': {'S': 'state2'},
+  'state2': {'R': 'state1', 'time': 'state0'},
+  'state3': {'time': 'state4'},
   'state4': {'M': 'state3', 'time': 10, 'next_state': 'state0'},
 }
 
 state = 'state0'
 on_enter_state0()
 
-start_time = time.time()
+
 
 while True:
   elapsed_time = time.time() - start_time
@@ -52,4 +67,3 @@ while True:
   else:
     print("Invalid state: " + state)
     break
-  time.sleep(1)
